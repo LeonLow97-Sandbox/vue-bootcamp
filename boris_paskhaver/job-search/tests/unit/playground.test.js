72996f4c24f40bb9f645a228c8b6de8b5136c144
LeonLow97 <lowjiewei@gmail.com>
2023-07-21 19:37:26 +0800

    Vitest on export functions

--- boris_paskhaver/job-search/tests/unit/playground.test.js
+++ boris_paskhaver/job-search/tests/unit/playground.test.js
@@ -1,7 +1,18 @@
 import { describe, it, expect } from 'vitest'
+import { evenOrOdd } from '@/playground'
 
 describe('basic math', () => {
   it('adds 2 numbers', () => {
     expect(1 + 1).toBe(2)
+  })
+
+  describe('evenOrOdd', () => {
+    it('number is even', () => {
+      expect(evenOrOdd(4)).toBe('Even')
+    })
+
+    it('number is odd', () => {
+      expect(evenOrOdd(3)).toBe('Odd')
+    })
   })
 })
