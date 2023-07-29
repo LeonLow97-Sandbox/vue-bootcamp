224c749ced37e0632e38802f8df72cd1bec0e32a
LeonLow97 <lowjiewei@gmail.com>
2023-07-29 22:45:22 +0800

    Adding beforeEach and afterEach

--- boris_paskhaver/job-search/tests/unit/components/TheHeadline.test.js
+++ boris_paskhaver/job-search/tests/unit/components/TheHeadline.test.js
@@ -1,29 +1,60 @@
+import { nextTick } from 'vue'
 import { render, screen } from '@testing-library/vue'
 
 import TheHeadline from '@/components/TheHeadline.vue'
+import { afterEach } from 'vitest'
 
 describe('TheHeadline', () => {
-  it("displays introductory action verb", () => {
+  beforeEach(() => {
     vi.useFakeTimers() // use fake timers to control time-related operations
-    render(TheHeadline);
-    
-    const actionPhrase = screen.getByRole("heading", {
-        name: /build for everyone/i,
-    })
-    expect(actionPhrase).toBeInTheDocument();
+  })
 
+  afterEach(() => {
     vi.useRealTimers() // clean up function for timers (reset to JavaScript timer)
   })
 
-  it("changes action verb at a consistent interval", () => {
-    vi.useFakeTimers()
+  it('displays introductory action verb', () => {
+    render(TheHeadline)
+
+    const actionPhrase = screen.getByRole('heading', {
+      name: /build for everyone/i
+    })
+    expect(actionPhrase).toBeInTheDocument()
+  })
+
+  it('changes action verb at a consistent interval', () => {
     const mock = vi.fn() // creating a mock function
-    vi.stubGlobal("setInterval", mock) // replacing global name with whatever we want
+    vi.stubGlobal('setInterval', mock) // replacing global name with whatever we want
 
     render(TheHeadline)
 
-    expect(mock).toHaveBeenCalled();
+    expect(mock).toHaveBeenCalled()
 
-    vi.useRealTimers()
+    vi.unstubAllGlobals() // removes setInterval mock and returns it back to the default JSDOM implementation
+  })
+
+  it('swaps action verb after interval', async () => {
+    render(TheHeadline)
+    vi.advanceTimersToNextTimer() // move setInterval to the next interval
+
+    await nextTick() // asynchronous function (moment when Vue updates DOM)
+
+    const actionPhrase = screen.getByRole('heading', {
+      name: /create for everyone/i
+    })
+
+    expect(actionPhrase).toBeInTheDocument()
+  })
+
+  it('removes interval when component disappears/unmounts', () => {
+    const clearInterval = vi.fn()
+    vi.stubGlobal('clearInterval', clearInterval)
+
+    const { unmount } = render(TheHeadline)
+    unmount() // simulate unmounting of component
+
+    expect(clearInterval).toHaveBeenCalled()
+
+    vi.unstubAllGlobals() // removes clearInterval mock and returns it back to the default JSDOM implementation
   })
 })
