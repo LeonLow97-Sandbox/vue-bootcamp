e7d8a79b894633ce2c0d3c0a98819ebfccf81f35
LeonLow97 <lowjiewei@gmail.com>
2023-07-31 22:07:37 +0800

    Writing Test for JobListing component with prop

---
+++ boris_paskhaver/job-search/tests/unit/components/JobResults/JobListing.test.js
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/vue'
+import { RouterLinkStub } from '@vue/test-utils'
+
+import JobListing from '@/components/JobResults/JobListing.vue'
+
+describe('JobListing', () => {
+  const createJobProps = (jobProps = {}) => ({
+    title: 'Vue Developer',
+    organization: 'AirBnB',
+    ...jobProps
+  })
+
+  const renderJobListing = (jobProps) => {
+    render(JobListing, {
+      global: {
+        stubs: {
+          'router-link': RouterLinkStub
+        }
+      },
+      props: {
+        job: {
+          ...jobProps
+        }
+      }
+    })
+  }
+
+  it('renders job title', () => {
+    const jobProps = createJobProps({ title: 'Vue Programmer' })
+    renderJobListing(jobProps)
+    expect(screen.getByText('Vue Programmer')).toBeInTheDocument()
+  })
+
+  it('renders job organizaiton', () => {
+    const jobProps = createJobProps({ organization: 'Google' })
+    renderJobListing(jobProps)
+    expect(screen.getByText('Google')).toBeInTheDocument()
+  })
+})
